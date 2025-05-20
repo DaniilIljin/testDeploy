99d66f92f5dba2140dc5b689d84f29f256f48e5f
Daniil Iljin <d.iljn.nkl@gmail.com>
2025-05-20 23:24:10 +0300

    cube geometry done I guess

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,11 +7,18 @@ export default function Home() {
   return (
     <>
       <Canvas>
-        <directionalLight position={[0, 0, 3]} />
-        <Cube position={[1, 1, 1]} side={1} color="orange"/>
-        <Cube position={[1, -1, 1]} side={1} color="green"/>
-        <Cube position={[-1, -1, 1]} side={1} color="red"/>
-        <Cube position={[-1, 1, 1]} side={1} color="purple"/>
+        <directionalLight position={[0, 0, 3]} intensity={5} />
+
+
+        {/* <group>
+          <Cube position={[1, -1, 1]} side={1} color="green" />
+          <Cube position={[1, 1, 1]} side={1} color="orange" />
+          <Cube position={[-1, -1, 1]} side={1} color="red" />
+          <Cube position={[-1, 1, 1]} side={1} color="purple" />
+        </group> */}
+
+      <Cube position={[0,0,0]} side={[2,2,2]} color="orange"/>
+
       </Canvas>
     </>
   );
--- src/component/Cube.tsx
+++ src/component/Cube.tsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import { useFrame } from "@react-three/fiber"
+import { log } from "console"
+import { useRef } from "react"
 
 type Props = {
   position: [x: number, y:number, z:number],
-  side: number,
+  side: [width: number, height: number, depth: number],
   color: string
 }
 
 const Cube = ({position, side, color} : Props) => {
+  
+  const ref = useRef()
+  useFrame((state, delta) => {
+    ref.current.rotation.x += delta
+    ref.current.rotation.y += delta
+    
+  })
+
   return <>
-    <mesh position={position}>
-      <boxGeometry args={[side]} />
+    <mesh position={position} ref={ref}>
+      <boxGeometry args={side} />
       <meshStandardMaterial color={color} />
     </mesh>
   </>
