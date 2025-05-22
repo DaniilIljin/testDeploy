c7c62c5740d96fc1e032673d143171f9315772bf
Daniil Iljin <d.iljn.nkl@gmail.com>
2025-05-22 23:10:06 +0300

    push test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,19 +3,14 @@
 import { Canvas } from "@react-three/fiber";
 import Cube from "@/component/Cube";
 import { OrbitControls, useHelper } from "@react-three/drei";
-import { DirectionalLightHelper } from "three";
 import { Leva } from "leva";
 import { Stars } from "@react-three/drei";
 
 const Scene = () => {
-  const directionLightRef = useHelper()
-  useHelper(directionLightRef, DirectionalLightHelper, 0.5, "white")
-
   return <>
     <directionalLight position={[2, 2, 3]} intensity={5}
-      ref={directionLightRef}
     />
-    <ambientLight intensity={0.1} />
+    <ambientLight intensity={0.3} />
     <Cube />
     <OrbitControls />
   </>
